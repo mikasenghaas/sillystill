01f4337dbd8fa87c9e9ebeb9addd58b177b37002
Mika Senghaas <mail@mikasenghaas.de>
2024-05-10 17:15:23 +0200

    feat(eval) pushed skeleton lightning eval metrics

---
+++ src/eval/psnr.py
@@ -0,0 +1,13 @@
+from torch import Tensor
+from torchmetrics import Metric
+
+class PSNRMetric(Metric):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        pass
+
+    def update(self, preds: Tensor, target: Tensor) -> None:
+        pass
+
+    def compute(self) -> Tensor:
+        pass
