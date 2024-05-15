618001c54b01b3561c11092ff2b3d11c0aaa292b
Mika Senghaas <mail@mikasenghaas.de>
2024-05-15 15:45:54 +0200

    feat(sh) updated run script

--- run.sh
+++ run.sh
@@ -1,11 +1,11 @@
 #!/bin/bash -l
 #SBATCH --ntasks 1
 #SBATCH --cpus-per-task 1
-#SBATCH --mem 50G
-#SBATCH --time 1:00:00
+#SBATCH --mem 32G
+#SBATCH --time 3:00:00
 #SBATCH --gres gpu:1
-#SBATCH --account cs413
-#SBATCH --qos cs413
+#SBATCH --account cs-552
+#SBATCH --qos cs-552
 #SBATCH --output=logs/slurm/slurm-%j.out
 
 # Load modules
@@ -14,8 +14,14 @@ module load gcc python
 # Activate venv
 source ~/venvs/sillystill/bin/activate
 
-# Run Python script
-python src/train.py -m experiment=translation
+# Install requirements
+pip install -r requirements.txt
+
+# Other available experiments
+# python src/train.py -m experiment=translation-net
+# python src/train.py -m experiment=translation-patch
+# python src/train.py -m experiment=translation-loss
+# python src/train.py -m experiment=translation-augment
 
 # Deactive venv
 deactivate
