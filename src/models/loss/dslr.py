7296c18baeb76106640f8239a102152ce09eb8f7
Pierre Lardet <lardet.pierre@gmail.com>
2024-05-22 13:42:24 +0200

    feat: added color and dslr loss

--- src/models/loss/dslr.py
+++ src/models/loss/dslr.py
@@ -1,5 +1,9 @@
+from typing import Literal
 import torch.nn.functional as F
 from torch import nn
+from .grain import GrainLoss
+from .color import ColorLoss
+from .mse_vgg import MSEVGGLoss
 
 
 class DSLRLoss(nn.Module):
@@ -10,11 +14,29 @@ class DSLRLoss(nn.Module):
     TF code: https://github.com/aiff22/DPED
 
     Contains 4 elements:
-    - Colour loss:
-    - Texture loss
-    - Content loss
-    - Total variationa loss
+    - Colour loss: Blurred colour channels MSE
+    - Texture loss: NOT YET IMPLEMENTED
+    - Content loss: VGG loss
+    - Total variational loss: Grain loss
     """
+
+    def __init__(
+        self,
+        color_weight=1.0,
+        tv_weight=1.0,
+        vgg_weight=1.0,
+        grain_loss_mode=Literal["max", "min", "diff"],
+        *args,
+        **kwargs
+    ) -> None:
+        super().__init__(*args, **kwargs)
+        self.color_loss = ColorLoss()
+        self.tv_los = GrainLoss(mode=grain_loss_mode)
+        self.vgg_loss = MSEVGGLoss(mse_weight=0.0, feature_weight=1.0)
+
+        self.color_weight = color_weight
+        self.tv_weight = tv_weight
+        self.vgg_weight = vgg_weight
 
     def forward(self, y, y_hat):
         """Compute the loss.
@@ -26,8 +48,20 @@ class DSLRLoss(nn.Module):
         Returns:
             loss: dict of the computed loss value.
         """
+        # Colour loss
+        color_loss = self.color_loss(y, y_hat)
 
-        # Compute the reconstruction loss
-        loss = F.mse_loss(y, y_hat)
+        # Texture loss
+        tv_loss = self.tv_los(y, y_hat)
+
+        # Content loss
+        vgg_loss = self.vgg_loss(y, y_hat)
+
+        # Total loss
+        loss = (
+            self.color_weight * color_loss
+            + self.tv_weight * tv_loss
+            + self.vgg_weight * vgg_loss
+        )
 
         return {"loss": loss}
