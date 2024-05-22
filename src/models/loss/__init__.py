deb98c083aca639196315fc914ca9da31eaa15f4
Pierre Lardet <lardet.pierre@gmail.com>
2024-05-22 12:55:20 +0200

    feat: add mse vgg grain loss

--- src/models/loss/__init__.py
+++ src/models/loss/__init__.py
@@ -2,3 +2,5 @@ from .cobi import CoBiLoss
 from .auto_translate import AutoTranslateLoss
 from .mse import MSELoss
 from .mse_vgg import MSEVGGLoss
+from .grain import GrainLoss
+from .mse_vgg_grain import MSEVGGGrainLoss
